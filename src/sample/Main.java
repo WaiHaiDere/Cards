29eb62838709bf4dace09d25391deaf911f2a854
Tyger Kong <tyger.kong@hotmail.com>
2019-01-23 01:18:44 +1300

    Added Icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Flash Cards");
+        primaryStage.getIcons().add(new Image("https://avatars3.githubusercontent.com/u/40849970?s=400&u=66111338b468e5a46fe1476b406e7640adcf066a&v=4"));
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
